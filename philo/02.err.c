74f2f5f6ecea918b7565f6bde94c9552b492431a
jongoh lee <sky40165285@gmail.com>
2023-08-08 13:13:23 +0900

    asdf

---
+++ philo/02.err.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   02.err.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jongolee <jongolee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/21 19:02:51 by jongolee          #+#    #+#             */
+/*   Updated: 2023/08/08 02:44:10 by jongolee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./main.h"
+
+void	on_error(int ERROR_CODE)
+{
+	if (ERROR_CODE == MALLOC)
+		printf("Error\nmalloc failed\n");
+	else if (ERROR_CODE == INPUT)
+		printf("Error\ninput in valid\n");
+	else if (ERROR_CODE == ARG)
+	{
+		printf(
+		"[argument format] 1 2 3 4 [5]\n"
+		"1)number_of_philosophers\n"
+		"2)time_to_die(ms)\n"
+		"3)time_to_eat(ms)\n"
+		"4)time_to_sleep(ms)\n"
+		"[5]number_of_times_each_philosopher_must_eat\n");	
+	}
+	exit(EXIT_FAILURE);
+}
---
+++ philo/main.h
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jongolee <jongolee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/21 18:59:58 by jongolee          #+#    #+#             */
+/*   Updated: 2023/08/08 13:07:19 by jongolee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_H
+# define MAIN_H
+# include <pthread.h>
+# include <stdio.h>
+# include <sys/time.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+enum e_state {
+	THINKING, 
+	EATING, 
+	SLEEPING, 
+	DEAD,
+	DONE
+};
+
+enum e_error {
+	MALLOC,
+	INPUT,
+	ARG
+};
+
+typedef struct s_data
+{
+	//개별
+	int					id;
+	long long			start_time;
+	//기본 정보
+	int					time_to_die;
+	int					eating_time;
+	int					sleeping_time;
+	int					must_eat;
+	int					eat_counter;
+	char				*fork;
+	//모니터링
+	int					philo_num;
+	int					full_philo;
+	int					over; > 다 먹거나 한명 죽거나
+	pthread_mutex_t		id_mutex;
+}	t_data;
+
+void	on_error(int ERROR_CODE);
+int		is_input_valid(char *str, int i, int sign);
+int		ft_atoi(char *str);
+void	print_died(unsigned long sec, unsigned long usec, int id);
+#endif
